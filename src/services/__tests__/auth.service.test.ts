08955cd726562d2c60f5052a90a5797e79f3bf52
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2025-04-25 17:02:32 -0300

    test: update auth service tests

--- src/services/__tests__/auth.service.test.ts
+++ src/services/__tests__/auth.service.test.ts
@@ -1,33 +1,33 @@
-import { AuthService } from "../../services/auth.service";
-import { User } from "../../models/User";
-import bcrypt from "bcrypt";
-import * as jwtUtils from "../../utils/jwt.utils";
-import * as cpfUtils from "../../utils/cpf.utils";
-import { RegisterDTO } from "../../dtos/RegisterDTO";
-import { HttpError } from "../../errors/HttpError";
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/User';
+import bcrypt from 'bcrypt';
+import * as jwtUtils from '../../utils/jwt.utils';
+import * as cpfUtils from '../../utils/cpf.utils';
+import { RegisterDTO } from '../../dtos/RegisterDTO';
+import { HttpError } from '../../errors/HttpError';
 
-jest.mock("../../models/User");
-jest.mock("bcrypt");
-jest.mock("../../utils/jwt.utils");
-jest.mock("../../utils/cpf.utils");
+jest.mock('../../models/User');
+jest.mock('bcrypt');
+jest.mock('../../utils/jwt.utils');
+jest.mock('../../utils/cpf.utils');
 
-describe("AuthService", () => {
+describe('AuthService', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  describe("register", () => {
-    it("should create a user and return public data", async () => {
+  describe('register', () => {
+    it('should create a user and return public data', async () => {
       const mockData: RegisterDTO = {
-        name: "John Doe",
-        email: "john@example.com",
-        password: "123456",
-        cpf: "12345678901",
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: '123456',
+        cpf: '12345678901',
       };
 
       (User.findOne as jest.Mock).mockResolvedValue(null);
       (cpfUtils.isValidCPFFormat as jest.Mock).mockReturnValue(true);
-      (bcrypt.hash as jest.Mock).mockResolvedValue("hashed_password");
+      (bcrypt.hash as jest.Mock).mockResolvedValue('hashed_password');
       (User.create as jest.Mock).mockResolvedValue({
         id: 1,
         name: mockData.name,
@@ -47,104 +47,107 @@ describe("AuthService", () => {
       });
     });
 
-    it("should throw HttpError if email is already registered", async () => {
+    it('should throw HttpError if email is already registered', async () => {
       (User.findOne as jest.Mock).mockResolvedValue({ id: 1 });
 
       await expect(
         AuthService.register({
-          name: "Test",
-          email: "test@example.com",
-          password: "pass",
-          cpf: "12345678901",
+          name: 'Test',
+          email: 'test@example.com',
+          password: 'pass',
+          cpf: '12345678901',
         })
       ).rejects.toThrow(HttpError);
 
       await expect(
         AuthService.register({
-          name: "Test",
-          email: "test@example.com",
-          password: "pass",
-          cpf: "12345678901",
+          name: 'Test',
+          email: 'test@example.com',
+          password: 'pass',
+          cpf: '12345678901',
         })
       ).rejects.toMatchObject({
-        message: "Email already registered.",
+        message: 'Email already registered.',
         statusCode: 400,
       });
     });
 
-    it("should throw HttpError if CPF format is invalid", async () => {
+    it('should throw HttpError if CPF format is invalid', async () => {
       (User.findOne as jest.Mock).mockResolvedValue(null);
       (cpfUtils.isValidCPFFormat as jest.Mock).mockReturnValue(false);
 
       await expect(
         AuthService.register({
-          name: "Test",
-          email: "test@example.com",
-          password: "pass",
-          cpf: "invalid-cpf",
+          name: 'Test',
+          email: 'test@example.com',
+          password: 'pass',
+          cpf: 'invalid-cpf',
         })
       ).rejects.toThrow(HttpError);
 
       await expect(
         AuthService.register({
-          name: "Test",
-          email: "test@example.com",
-          password: "pass",
-          cpf: "invalid-cpf",
+          name: 'Test',
+          email: 'test@example.com',
+          password: 'pass',
+          cpf: 'invalid-cpf',
         })
       ).rejects.toMatchObject({
-        message: "CPF is invalid.",
+        message: 'CPF is invalid.',
         statusCode: 400,
       });
     });
   });
 
-  describe("login", () => {
-    it("should login a user and return a token", async () => {
-      const loginData = { email: "john@example.com", password: "123456" };
+  describe('login', () => {
+    it('should login a user and return a token', async () => {
+      const loginData = { email: 'john@example.com', password: '123456' };
 
       const userMock = {
         id: 1,
         email: loginData.email,
-        password: "hashed_password",
+        password: 'hashed_password',
       };
 
       (User.findOne as jest.Mock).mockResolvedValue(userMock);
       (bcrypt.compare as jest.Mock).mockResolvedValue(true);
-      jest.spyOn(jwtUtils, "generateToken").mockReturnValue("fake_token");
+      jest.spyOn(jwtUtils, 'generateToken').mockReturnValue('fake_token');
 
       const result = await AuthService.login(loginData);
 
-      expect(result).toEqual({ token: "fake_token" });
+      expect(result).toEqual({ token: 'fake_token' });
     });
 
-    it("should throw HttpError if user not found", async () => {
+    it('should throw HttpError if user not found', async () => {
       (User.findOne as jest.Mock).mockResolvedValue(null);
 
       await expect(
-        AuthService.login({ email: "x@x.com", password: "123" })
+        AuthService.login({ email: 'x@x.com', password: '123' })
       ).rejects.toThrow(HttpError);
 
       await expect(
-        AuthService.login({ email: "x@x.com", password: "123" })
-      ).rejects.toMatchObject({ message: "User not found.", statusCode: 404 });
+        AuthService.login({ email: 'x@x.com', password: '123' })
+      ).rejects.toMatchObject({
+        message: 'User or password are invalid.',
+        statusCode: 400,
+      });
     });
 
-    it("should throw if password is invalid", async () => {
+    it('should throw if password is invalid', async () => {
       (User.findOne as jest.Mock).mockResolvedValue({
-        email: "x@x.com",
-        password: "hashed",
+        email: 'x@x.com',
+        password: 'hashed',
       });
       (bcrypt.compare as jest.Mock).mockResolvedValue(false);
 
       await expect(
-        AuthService.login({ email: "x@x.com", password: "wrong" })
+        AuthService.login({ email: 'x@x.com', password: 'wrong' })
       ).rejects.toThrow(HttpError);
 
       await expect(
-        AuthService.login({ email: "x@x.com", password: "123" })
+        AuthService.login({ email: 'x@x.com', password: '123' })
       ).rejects.toMatchObject({
-        message: "Invalid password.",
+        message: 'User or password are invalid.',
         statusCode: 400,
       });
     });
