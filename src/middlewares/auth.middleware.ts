075d3695acaa4dfea50a8ee99828f3f3c33e3fd7
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2025-04-25 12:57:55 -0300

    fix: add next() on auth middleware to call next

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,8 @@
-import { EnvironmentVariableMissing } from "../errors/EnvironmentVariableMissing";
-import { HttpError } from "../errors/HttpError";
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import { Error } from "sequelize";
+import { EnvironmentVariableMissing } from '../errors/EnvironmentVariableMissing';
+import { HttpError } from '../errors/HttpError';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { Error } from 'sequelize';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -12,22 +12,24 @@ export const authenticateToken = (
   next: NextFunction
 ): void => {
   try {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-      throw new HttpError("Access token required", 401);
+      throw new HttpError('Access token required', 401);
     }
 
     if (!JWT_SECRET) {
-      throw new EnvironmentVariableMissing("JWT_SECRET");
+      throw new EnvironmentVariableMissing('JWT_SECRET');
     }
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
       if (err) {
-        throw new HttpError("Invalid or expired token", 403);
+        throw new HttpError('Invalid or expired token', 403);
       }
     });
+
+    next();
   } catch (erro) {
     next(erro);
   }
